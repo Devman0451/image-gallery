947258018bff97763bfbbaee8b361d0fa3b5b5e5
Justin Myers <42520471+Devman0451@users.noreply.github.com>
2018-12-02 23:48:07 -0500

    minor fixes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,14 +12,14 @@ const footer = () => (
                 <li><a href="#!">Create Account</a></li>
             </ul>
             <ul className={styles["FooterColumn2"]}>
-                <li><a href="https://www.linkedin.com/in/justin-myers-712901173/" target="_blank"><i className="fab fa-linkedin-in"></i></a></li>
-                <li><a href="https://tender-varahamihira-ff1ef7.netlify.com/" target="_blank"><i className="fas fa-home"></i></a></li>
-                <li><a href="https://github.com/Devman0451" target="_blank"><i className="fab fa-github"></i></a></li>
+                <li><a href="https://www.linkedin.com/in/justin-myers-712901173/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin-in"></i></a></li>
+                <li><a href="https://tender-varahamihira-ff1ef7.netlify.com/" target="_blank" rel="noopener noreferrer"><i className="fas fa-home"></i></a></li>
+                <li><a href="https://github.com/Devman0451" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a></li>
             </ul>
             <ul className={styles["FooterColumn3"]}>
-                <li><a href="https://www.linkedin.com/in/justin-myers-712901173/" target="_blank">Linkedin</a></li>
-                <li><a href="https://tender-varahamihira-ff1ef7.netlify.com/" target="_blank">My Portfolio Site</a></li>
-                <li><a href="https://github.com/Devman0451" target="_blank">My Github</a></li>
+                <li><a href="https://www.linkedin.com/in/justin-myers-712901173/" target="_blank" rel="noopener noreferrer">Linkedin</a></li>
+                <li><a href="https://tender-varahamihira-ff1ef7.netlify.com/" target="_blank" rel="noopener noreferrer">My Portfolio Site</a></li>
+                <li><a href="https://github.com/Devman0451" target="_blank" rel="noopener noreferrer">My Github</a></li>
             </ul>
         </div>
         <h2>Newsletter</h2>
