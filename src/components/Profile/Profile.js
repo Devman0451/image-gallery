3cc5ea9cabb977755e5e7f078bdc18e1452dc158
Justin Myers <42520471+Devman0451@users.noreply.github.com>
2018-12-13 04:33:07 -0500

    basic profile page

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchShowcaseProfile } from '../../store/profileActions';
 
-const profile = props => {
-    return (
-        <h1>Profile</h1>
-    );
+import styles from './Profile.module.css';
+
+const imgPath = process.env.PUBLIC_URL + '/assets/images/';
+
+class Profile extends Component {
+    
+    componentDidMount() {
+        if(this.props.profile == null) {
+            this.props.fetchProfile(this.props.match.params.profile_id);
+        }
+        console.log(this.props.profile);
+    }
+
+    render() {
+        const title = this.props.profile ? this.props.profile.name : "Profile";
+        const handle = this.props.profile ? this.props.profile.handle : "@handle";
+        return (
+            <>
+            <div className={styles["ProfileBanner"]}>
+                <img src={imgPath + "artist.jpg"} alt="artist" className={styles["ProfileImage"]} />
+                <h1>{title}</h1>
+                <h3>{handle}</h3>
+            </div>
+            </>
+        );
+    }
 }
 
-export default profile;
+const mapStateToProps = state => {
+    return {
+        profile: state.profile.showcaseProfile
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        fetchProfile: (profile) => {dispatch(fetchShowcaseProfile(profile))}
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Profile);
